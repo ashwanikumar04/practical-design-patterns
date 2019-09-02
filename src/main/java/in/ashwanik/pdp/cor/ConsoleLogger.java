0b901ac9ec72035d1b80a6c18e21b6eede355aca
Ashwani Kumar <ashwanikumar04@gmail.com>
2019-09-02 20:32:59 +0100

    added chain of responsibility

---
+++ src/main/java/in/ashwanik/pdp/cor/ConsoleLogger.java
@@ -0,0 +1,18 @@
+package in.ashwanik.pdp.cor;
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+class ConsoleLogger extends AbstractLogger {
+
+    ConsoleLogger(OutputStream outputStream, int level) {
+        super(outputStream, level);
+    }
+
+    @Override
+    void write(String message) throws IOException {
+        outputStream.write(("console: " + message).getBytes());
+        outputStream.write("\n".getBytes());
+    }
+}
---
+++ src/test/java/in/ashwanik/pdp/cor/CORLoggerUnitTest.java
@@ -0,0 +1,30 @@
+package in.ashwanik.pdp.cor;
+
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CORLoggerUnitTest {
+    @Test
+    public void testLogger() throws IOException {
+        //Arrange
+        OutputStream outputStream = new ByteArrayOutputStream();
+        FileLogger fileLogger = new FileLogger(outputStream, AbstractLogger.ERROR);
+        ConsoleLogger consoleLogger = new ConsoleLogger(outputStream, AbstractLogger.DEBUG);
+        fileLogger.setNextLogger(consoleLogger);
+
+        //Act
+        fileLogger.logMessage(AbstractLogger.ERROR, "This is error");
+        fileLogger.logMessage(AbstractLogger.INFO, "This is info");
+        fileLogger.logMessage(AbstractLogger.DEBUG, "This is debug");
+        String messages = outputStream.toString();
+
+        //Assert
+        assertThat(messages).doesNotContain("file:This is info");
+    }
+}
